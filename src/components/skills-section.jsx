fd429a409849772bec7fcb38f2e87f54cf92c60e
Kenta Young <kenta@kentayoung.dev>
2024-11-04 22:13:44 -0700

    feat: update SkillsSection to include Shopify skill and adjust progress bar percentages

--- src/components/skills-section.jsx
+++ src/components/skills-section.jsx
@@ -2,14 +2,14 @@ import ProgressBar from './progressBar';
 
 const SkillsSection = () => {
 	return (
-		<section id='skills' className='section-container'>
-			<h2 className='section-header'>Skills</h2>
-			<ProgressBar percent='95' title='Front End Development' />
-      <ProgressBar percent='70' title='Back End Development' />
-      <ProgressBar percent='90' title='Debugging' />
-      <ProgressBar percent='85' title='Agile' />
-		</section>
-	);
+    <section id="skills" className="section-container">
+      <h2 className="section-header">Skills</h2>
+      <ProgressBar percent="95" title="Front End Development" />
+      <ProgressBar percent="95" title="Shopify" />
+      <ProgressBar percent="70" title="Back End Development" />
+      <ProgressBar percent="90" title="Debugging" />
+    </section>
+  );
 };
 
 export default SkillsSection;
