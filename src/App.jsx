097b42593a5a8f46151dab80e2bd7643a5da96f3
Kenta Young <kenta@kentayoung.dev>
2024-11-04 22:03:32 -0700

    refactor: remove unused variables and console logs from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import './css/kentaYoungWeb.scss';
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import firebase from './config/firebaseConfig';
 import { getAnalytics } from 'firebase/analytics';
@@ -10,13 +9,11 @@ import Navbar from './components/navbar/navbar';
 import AnimatedRoutes from './routes/animatedRoutes';
 
 function App() {
-	let welcomeMessageLogged = false;
 	const updateViewportHeight = () => {
 		document.documentElement.style.setProperty('--viewport-height', `${window.innerHeight}px`);
 	};
 	window.addEventListener('resize', updateViewportHeight);
 	updateViewportHeight();
-	console.log(import.meta.env.MODE);
 	if (import.meta.env.MODE !== "development") {
     //setting up analytics for prod
     getAnalytics(firebase);
@@ -25,7 +22,6 @@ function App() {
   }
 
 	const consoleWelcomeMessage = () => {
-		if (welcomeMessageLogged) return;
 		const consoleStyleMain = 'font-size: 14px;';
 		const consoleStyleChild = 'font-size: 12px;';
 		console.group(`%c👋 Fancy seeing you here. Don't forget to check out my other links.`, consoleStyleMain);
